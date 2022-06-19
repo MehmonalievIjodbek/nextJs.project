ec28f1be7df2460c6d79d2214fb11b41eb84ed3e
IjodbekMehmonaliyev03 <IjodbekMehmonaliyev@gmail.com>
2022-06-19 22:45:57 +0500

    navbarJs is finished

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,53 +2,70 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link'
 
-import React from 'react'
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <Link href='/'>
-            <a>
-                <img  src="/images/logo.svg"
-                alt='tesla logo'
-                loading='lazy'
-                className={styling.teslalogo}
-                />
-            </a>
+                <a>
+                    <img src="/images/logo.svg"
+                         alt='tesla logo'
+                         loading='lazy'
+                         className={styles.teslaLogo}
+                    />
+                </a>
             </Link>
 
             <ul className={styles.navbarNav}>
-                <li className={styles.item}>
-                    <a href="#">
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink} >
                         Model S
                     </a>
                 </li>
-                <li className={styles.item}>
-                    <a href="#">
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
                         Model 3
                     </a>
                 </li>
-                <li className={styles.item}>
+                <li className={styles.items}>
                     <a href="#">
                         Model X
                     </a>
                 </li>
-                <li className={styles.item}>
-                    <a href="#">
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
                         Model Y
                     </a>
                 </li>
-                <li className={styles.item}>
-                    <a href="#">
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
                         Solar Roof
                     </a>
                 </li>
-                <li className={styles.item}>
-                    <a href="#">
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
                         Solar Roof
                     </a>
                 </li>
             </ul>
+
+            <div className={styles.menu}>
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
+                        Solar Roof
+                    </a>
+                </li>
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink}>
+                        Solar Roof
+                    </a>
+                </li>
+                <li className={styles.items}>
+                    <a href="#" className={styles.itemsLink} >
+                        Solar Roof
+                    </a>
+                </li>
+            </div>
         </nav>
     )
 }
