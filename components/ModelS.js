f6278990966acf06fe6a867997157a0b0d8ae8d4
IjodbekMehmonaliyev03 <IjodbekMehmonaliyev@gmail.com>
2022-06-20 00:42:54 +0500

    ModelS.JS, ModelX, ModelY, and SolarPanels is finished

---
+++ components/ModelS.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const ModelS = () => {
+    return (
+        <section className={styles.section}>
+            <div className={styles.textsArea}>
+                <h3>Model S</h3>
+                <p>Order Online fro <span className={styles.touchless}>Touchless delivery</span></p>
+            </div>
+            <div className={styles.buttons}>
+                <div className={styles.btnMain}>
+                    <a href="#" className={styles.btn}>
+                        custom order
+                    </a>
+                    <a href="#" className={styles.btn}>
+                        existing inventory
+                    </a>
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default ModelS;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Header from "../components/Header";
+import ModelY from "../components/ModelY";
+import ModelS from "../components/ModelS";
+import ModelX from "../components/ModelX";
+import SolarPanels from "../components/SolarPanels";
 
 export default function Home() {
-  return (
-   <>
-   <Head>
-    <title>Tesla Clone</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'></meta>
-   </Head>
-      <Navbar/>
-       <Header/>
-   </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Tesla Clone</title>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'></meta>
+            </Head>
+            <Navbar/>
+            <Header/>
+            <ModelY/>
+            <ModelS/>
+            <ModelX/>
+            <SolarPanels/ >
+        </>
+    )
 }
